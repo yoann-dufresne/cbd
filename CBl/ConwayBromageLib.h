169da143ca446de41c477db50caca673cbe462e9
muratokutucu <54741414+muratokutucu@users.noreply.github.com>
2020-06-08 18:26:36 +0200

    Update ConwayBromageLib.h

--- CBl/ConwayBromageLib.h
+++ CBl/ConwayBromageLib.h
@@ -17,10 +17,10 @@ sdsl::sd_vector<>fromFileToSdVector(std::string path);
 bool isThisKMerHere(std::string nonCompressedKMer, sdsl::sd_vector<> const& currentCompressedSeq); //Alexandra
 std::vector<std::string> next(std::string nonCompressedKMer, sdsl::sd_vector<> const& currentCompressedSeq);   //Murat
 std::vector<std::string> previous(std::string nonCompressedKMer, sdsl::sd_vector<> const& currentCompressedSeq);    //Alexandra
-std::vector<uint64_t> nextCompressed(std::string nonCompressedKMer, sdsl::sd_vector<> const& currentCompressedSeq); //Murat
-std::vector<uint64_t> previousCompressed(std::string nonCompressedKMer, sdsl::sd_vector<> const& currentCompressedSeq); //Alexandra
+std::vector<uint64_t> nextCompressed(uint64_t nonCompressedKMer, sdsl::sd_vector<> const& currentCompressedSeq); //Murat
+std::vector<uint64_t> previousCompressed(uint64_t nonCompressedKMer, sdsl::sd_vector<> const& currentCompressedSeq); //Alexandra
 
-//Can be usefull ?
+//Can be useful ?
 std::string successorOfOnes(std::string nonCompressedKMer, sdsl::sd_vector<> const& currentCompressedSeq);
 std::string predecessorOfOnes(std::string nonCompressedKMer, sdsl::sd_vector<> const& currentCompressedSeq);
 
