d23d5b583c29762227be751807c93b67d1669c82
AkumalexTheExalted <57628600+AkumalexTheExalted@users.noreply.github.com>
2020-06-08 17:08:29 +0200

    Add next and previous compressed mode prototype

--- CBl/ConwayBromageLib.h
+++ CBl/ConwayBromageLib.h
@@ -17,6 +17,8 @@ sdsl::sd_vector<>fromFileToSdVector(std::string path);
 bool isThisKMerHere(std::string nonCompressedKMer, sdsl::sd_vector<> const& currentCompressedSeq); //Alexandra
 std::vector<std::string> next(std::string nonCompressedKMer, sdsl::sd_vector<> const& currentCompressedSeq);   //Murat
 std::vector<std::string> previous(std::string nonCompressedKMer, sdsl::sd_vector<> const& currentCompressedSeq);    //Alexandra
+std::vector<uint64_t> nextCompressed(std::string nonCompressedKMer, sdsl::sd_vector<> const& currentCompressedSeq); //Murat
+std::vector<uint64_t> previousCompressed(std::string nonCompressedKMer, sdsl::sd_vector<> const& currentCompressedSeq); //Alexandra
 
 //Can be usefull ?
 std::string successorOfOnes(std::string nonCompressedKMer, sdsl::sd_vector<> const& currentCompressedSeq);
