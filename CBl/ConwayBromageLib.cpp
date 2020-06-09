f977f14e03030be1c1932edfaa551db3b0fae821
muratokutucu <54741414+muratokutucu@users.noreply.github.com>
2020-06-09 15:54:31 +0200

    Update ConwayBromageLib.cpp

--- CBl/ConwayBromageLib.cpp
+++ CBl/ConwayBromageLib.cpp
@@ -57,6 +57,31 @@ uint64_t select0bit(uint64_t index, const sd_vector<> &v){
     return sdb_sel(index);
 }
 
+/**
+ * The ecoli_count.txt file. Sort the k-mers with this order : A > C > T > G.
+ * @param word the k-mer
+ * @param size the size of the k-mer
+ * @return an integer representing the k-mer
+ */
+uint64_t encodeEcoli(string word, uint64_t size){
+    uint64_t hash = 0;
+    char c;
+    for(uint i = 0 ; i < size ; i++){   //We go through the sequence to encode each nucleotides
+        hash <<= 2; // We shift 2 bits to the right
+        c = word[i];    //Take each nucleotides to encode them
+        uint64_t charval = 0; // 'A' = 0
+        if(c == 'C'){
+            charval = 1;    // 'C' = 1
+        }else if(c == 'T'){
+            charval = 2;    // 'T' = 2
+        }else if(c == 'G'){
+            charval = 3;    // 'G' = 3
+        }
+        hash += charval;    //creation of the hash for the given sequence
+    }
+    return hash;    //return the final hash of the sequence
+}
+
 /* function to encode k-mer sequences
  * Need a string (the sequence) and a uint64_t which is the size of the sequence
  * return a uint64_t which is the encoding version of the sequence
@@ -226,6 +251,44 @@ vector<uint64_t> previousCompressed(uint64_t nonCompressedKMer, sdsl::sd_vector<
     }
     return prev;
 }
+
+/* Transform sequences which are contain in a file in a sd_vector
+ * Need a string which is the path to the file
+ * Return a sd_vector which contains encoding version of sequences of the file
+ */
+sd_vector<>fromFileToSdVectorEcoli(string path){
+    ifstream file(path, ios::in);  // Reading of the file which contains k-mers sequences
+    if(file){   // File is now open
+        string word;
+        string line("");
+        file >> word;   //Take the first word to analyze size of one k-mer
+        file.seekg(0, ios::beg);    //Return to the beginning of the file
+        int myWordLen(word.size()); //Size of k_mer, it is the 'k'
+        int myOneLen(0);
+        while(getline(file, line)){ //Counts the number of ones in the file
+            myOneLen++;
+        }
+        file.clear();
+        file.seekg(0, ios::beg);    //Return to he beginning of the file
+        cout << "length of ones : " << myOneLen << endl;
+        cout << "length of a seq : " << myWordLen << endl;
+        long int myTotalLen(pow(ALPHABET,myWordLen));   //Creation of the total length to create the sd_vector_builder
+        cout << "Total length : " << myTotalLen << endl;
+        sd_vector_builder constructSparse(myTotalLen, myOneLen);    //A size of myTotalLen, contains myOneLen ones
+        while(file >> word){
+            cout << "The seq is : " << word << endl;
+            constructSparse.set(encodeEcoli(word, myWordLen)); //filled to one each element which is represent by the encoding version of the sequence
+            file >> word;
+        }
+        sd_vector<>finalSparse(constructSparse);    //Construction of the final sd_vector
+        file.close();
+        return finalSparse;
+    }else{
+        cout << "Error while opening" << endl;
+    }
+    return bit_vector{0};
+}
+
 
 /* Transform sequences which are contain in a file in a sd_vector
  * Need a string which is the path to the file
