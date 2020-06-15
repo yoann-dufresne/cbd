16f77b47a4079bf41e641a0518fb8202dc70f024
AkumalexTheExalted <57628600+AkumalexTheExalted@users.noreply.github.com>
2020-06-15 11:15:39 +0200

    Modifications of previous tests

--- Tests.cpp
+++ Tests.cpp
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-/*
+/* 
  * File:   Tests.cpp
  * Author: muratokutucu
  *
@@ -27,7 +27,7 @@ using namespace lest;
 
 static const uint64_t  totalLen = 4611686018427387904;
 static const sd_vector<>littleTestPrev = bit_vector{0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1};
-
+static const sd_vector<> ret = fromFileToSdVector("./sorted_kmers.txt");
 /*
  * Compilation line : g++ -Wall -Wextra -std=c++11 -Dlest_FEATURE_AUTO_REGISTER=1 -Dlest_FEATURE_COLOURISE=1 -O3 -DNDEBUG -I ~/include -L ~/lib -o Tests.exe Tests.cpp ConwayBromageLib.cpp -lsdsl -ldivsufsort -ldivsufsort64
  */
@@ -35,53 +35,123 @@ static const sd_vector<>littleTestPrev = bit_vector{0, 1, 0, 1, 1, 1, 0, 0, 1, 0
 //Appears to other tests, need to be verify in first
 const test critical[] = {
         CASE("merge : "){
-            bit_vector ba = {0, 1, 1, 1, 0, 1};
-            bit_vector bb = {1, 0, 0, 0};
-            sd_vector<>a(ba);
-            sd_vector<>b(bb);
-            sd_vector<> res = merge(a, b, 4, 1);
-            uint64_t finalSize(0);
-            if(a.size() > b.size()){
-                finalSize = a.size();
-            }else{
-                finalSize = b.size();
-            }
-            EXPECT(res.size() == finalSize);
-            for(int i = 0 ; i < finalSize ; i++){
-                if(a[i] || b[i]){
-                    EXPECT(res[i] == 1);
+            cout << "\t--> merge" << endl;
+                bit_vector ba = {0, 1, 1, 1, 0, 1};
+                bit_vector bb = {1, 0, 0, 0};
+                sd_vector<>a(ba);
+                sd_vector<>b(bb);
+                sd_vector<> res = merge(a, b, 4, 1);
+                uint64_t finalSize(0);
+                if(a.size() > b.size()){
+                    finalSize = a.size();
+                }else{
+                    finalSize = b.size();
                 }
-            }
+                EXPECT(res.size() == finalSize);
+                for(int i = 0 ; i < finalSize ; i++){
+                    if(a[i] || b[i]){
+                                EXPECT(res[i] == 1);
+                    }
+                }
         },
         //Tests of decode :
         CASE("decode case A = 0 ; C = 1 ; G = 2 ; T = 3 : original "){ //decode the 1000000 firsts elements of the generated sequence res (with fromFileToSdVector)
+            cout << "\t--> original decode" << endl;
             uint64_t len = log(totalLen) / log(ALPHABET);
             for(uint64_t i = 0 ; i < 1000000 ; i++){
-                EXPECT(i == encode(decode(i, len), len));   //expect to find i with a encode-decode combination
-                // cout << decode(i, len) << " : " << i << endl;
-
+                        EXPECT(i == encode(decode(i, len), len));   //expect to find i with a encode-decode combination
             }
         },
         //Tests of previous :
-        CASE("previous : perfect use expected for a manual sd_vector : "){  //Test with a perfect use of previous + a perfect generated sequence
-            vector<vector<uint64_t>> prevOfLittlePrev(16);
-            prevOfLittlePrev[1] = {4, 8};
-            prevOfLittlePrev[3] = {4, 8};
-            prevOfLittlePrev[4] = {1, 5, 13};
-            prevOfLittlePrev[5] = {1, 5, 13};
-            prevOfLittlePrev[13] = {3, 15};
-            prevOfLittlePrev[15] = {3, 15};
-            uint64_t currentKMerLen = log(littleTestPrev.size()) / log(ALPHABET);
-            for(int i = 0 ; i < littleTestPrev.size() ; i++){ //for the firsts 1000000
+        CASE("previous : perfect use expected without fromFileToSdVector call : "){  //Test with a perfect use of previous + a perfect generated sequence
+            cout << "\t--> previous with little sd_vector" << endl;
+            vector<vector<uint64_t>> prevOfLittlePrev(4);
+            prevOfLittlePrev[0] = { 1, 2 };
+            prevOfLittlePrev[1] = { 0, 1, 3 };
+            prevOfLittlePrev[3] = { 0, 3 };
+            for(int i = 0 ; i < 4 ; i++){
                 vector<uint64_t> prev = previous(i, littleTestPrev);
                 EXPECT(prevOfLittlePrev[i] == prev);
             }
         },
         CASE("previous : unexpected use : a non existant k-mer : "){    //Test with an example of bad use : try to find a non existing k-mer
+            cout << "\t--> previous for a non existant k-mer" << endl;
             vector<uint64_t> prev = previous(-404, littleTestPrev);    //a compressed k-mer can't have a negative number
-            EXPECT(prev.size() == 0);   //In this case, they is no possible previous
+                    EXPECT(prev.size() == 0);   //In this case, they is no possible previous
         },
-        //Test of next upon sorted_kmers.txt
+                //Test of next on a small sd_vector
+        CASE("next : small sd_vector"){
+            sd_vector<> sdv = bit_vector{0,1,0,1,1,1,0,0,1,0,0,0,0,1,0,1};
+            vector<vector<uint64_t>> TrueNext(16);
+            TrueNext[1]  = {4,   5};
+            TrueNext[3]  = {13, 15};
+            TrueNext[4]  = {1,   3};
+            TrueNext[5]  = {4,   5};
+            TrueNext[8]  = {1,   3};
+            TrueNext[13] = {4,   5};
+            TrueNext[15] = {13, 15};
+            bool result = true;
+            for(int i(0); i < sdv.size(); i++){
+                vector<uint64_t> successors = next(i, sdv);
+                //check if same
+                for(int j(0); j < successors.size(); j++){
+                    if(TrueNext[i].size() == successors.size()){
+                        for(int j = 0; j < successors.size(); j++){
+                            if(successors[j] != TrueNext[i][j]){
+                                result = false;
+                            }
+                        }
+                    } else {
+                        result = false;
+                    }
+                }
+            }
+            EXPECT(result);
+        },
+};
+
+const test lessCritical[] = {
+        CASE("reverse complement : faster lexicographical order"){
+            cout << "\t--> faster lexicographical order" << endl;
+            uint64_t len = log(totalLen) / log(ALPHABET);
+            for(int i = 0 ; i < 1000000 ; i++){
+                        EXPECT(i == reverseComplementLexico(reverseComplementLexico(i, len), len));
+            }
+        },
+};
+
+const test lessLessCrit[] = {
+        //Tests of fromFileToSdVector :
+        CASE("fromFileToSdVector : reading from a file : original "){ //verify os fromFileToSdVector can read a file correctly
+            cout << "\t--> original fromFileToSdVector" << endl;
+            EXPECT_NOT((ret.size() == 1 && ret[0] == 0));   //if res.size() == 1 and res[0] == 0, the file read has failed
+        },
+        CASE("fromFileToSdVector : reading from a file : original with reverse "){ //verify os fromFileToSdVector can read a file correctly
+            cout << "\t--> original fromFileToSdVector with reverse" << endl;
+            EXPECT_NOT((ret.size() == 1 && ret[0] == 0));   //if res.size() == 1 and res[0] == 0, the file read has failed
+        },
+};
+
+const test atTheEnd[] = {
+        CASE("previous : perfect use expected with file fromFileToSdVectorCall : "){  //Test with a perfect use of previous + a perfect generated sequence
+            cout << "\t--> previous with fromFileToSdVector call" << endl;
+            uint64_t currentKMerLen = log(ret.size()) / log(ALPHABET);
+            for (int i = 0; i < ret.size() / 4; i++) { //for the firsts 1000000
+                string current = decode(i, currentKMerLen-1);
+                cout << "current : " << i << endl;
+                vector<uint64_t> prev = previous(i, ret);
+                for(int j = 0 ; j < prev.size() ; j++){
+                    vector<uint64_t> proof;
+                    proof.push_back(encode((decode(prev[j], currentKMerLen-1).erase(0, 1)) + "A", currentKMerLen-1));
+                    proof.push_back(encode((decode(prev[j], currentKMerLen-1).erase(0, 1)) + "C", currentKMerLen-1));
+                    proof.push_back(encode((decode(prev[j], currentKMerLen-1).erase(0, 1)) + "G", currentKMerLen-1));
+                    proof.push_back(encode((decode(prev[j], currentKMerLen-1).erase(0, 1)) + "T", currentKMerLen-1));
+                    cout << "proof : " << proof << endl;
+                    EXPECT(i == proof[0] || i == proof[1] || i == proof[2] || i == proof[3]);
+                }
+            }
+        },
+                //Test of next upon sorted_kmers.txt
         CASE("next : test upon sorted_kmers.txt "){
             sd_vector<> sdv = fromFileToSdVector("./sorted_kmers.txt");
             int currentKMerLen = log(sdv.size()) / log(ALPHABET);
@@ -116,78 +186,29 @@ const test critical[] = {
                 EXPECT(result);
             }
         },
-
-        //Test of next on a small sd_vector
-        CASE("next : small sd_vector"){
-            sd_vector<> sdv = bit_vector{0,1,0,1,1,1,0,0,1,0,0,0,0,1,0,1};
-            vector<vector<uint64_t>> TrueNext(16);
-            TrueNext[1]  = {4,   5};
-            TrueNext[3]  = {13, 15};
-            TrueNext[4]  = {1,   3};
-            TrueNext[5]  = {4,   5};
-            TrueNext[8]  = {1,   3};
-            TrueNext[13] = {4,   5};
-            TrueNext[15] = {13, 15};
-            bool result = true;
-            for(int i(0); i < sdv.size(); i++){
-                vector<uint64_t> successors = next(i, sdv);
-                //check if same
-                for(int j(0); j < successors.size(); j++){
-                    if(TrueNext[i].size() == successors.size()){
-                        for(int j = 0; j < successors.size(); j++){
-                            if(successors[j] != TrueNext[i][j]){
-                                result = false;
-                            }
-                        }
-                    } else {
-                        result = false;
-                    }
-                }
-            }
-            EXPECT(result);
-        }
-};
-
-const test lessCritical[] = {
-        CASE("reverse complement : original"){
-            uint64_t len = log(totalLen) / log(ALPHABET);
-            for(int i = 0 ; i < 1000000 ; i++){
-                EXPECT(i == reverseComplement(decode(reverseComplement(decode(i, len), totalLen), len), totalLen));
-            }
-        },
-        CASE("reverse complement : faster lexicographical order"){
-            uint64_t len = log(totalLen) / log(ALPHABET);
-            for(int i = 0 ; i < 1000000 ; i++){
-                EXPECT(i == reverseComplementLexico(reverseComplementLexico(i, len), len));
-            }
-        }
-};
-
-const test atTheEnd[] = {
-        //Tests of fromFileToSdVector :
-        CASE("fromFileToSdVector : reading from a file : original "){ //verify os fromFileToSdVector can read a file correctly
-            sd_vector<>res = fromFileToSdVector("./sorted_kmers.txt");
-            EXPECT_NOT((res.size() == 1 && res[0] == 0));   //if res.size() == 1 and res[0] == 0, the file read has failed
-        }
 };
 
 int main(){
     int failures(0);
-    cout << "*** First tests : merge, decode and previous ***" << endl;
+    cout << "*** First tests : merge, decode and previous without file call ***" << endl;
     if(failures = run(critical)){  //run all the test cases
         return failures;
     }
-    cout << "*** First tests passed ! ***" << endl;
+    cout << "*** First tests passed ! ***\n" << endl;
     cout << "*** Second tests : reverse complement ***" << endl;
     if (failures = run(lessCritical)){
         return failures;
     }
-    cout << "*** Second tests passed ! ***" << endl;
+    cout << "*** Second tests passed ! ***\n" << endl;
     cout << "*** Third tests : fromFileToSdVector ***" << endl;
+    if (failures = run(lessLessCrit)){
+        return failures;
+    }
+    cout << "*** Third tests passed ! ***\n" << endl;
+    cout << "*** Fourth tests : previous with fromFileToSdVector call ***" << endl;
     if (failures = run(atTheEnd)){
         return failures;
     }
-    cout << "*** Third tests passed ! ***" << endl;
+    cout << "*** Fourth tests passed ! ***\n" << endl;
     return cout << "ALL TESTS PASSED !!!\n", EXIT_SUCCESS;
 }
-
