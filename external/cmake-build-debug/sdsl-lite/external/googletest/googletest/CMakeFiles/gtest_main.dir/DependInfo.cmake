3bc6292d5a06cc89c3d1e73e1c05c3a869d17c2c
Alexandra <Alexandra>
2020-06-16 14:13:25 +0200

    submodule implementation + new CMake

---
+++ external/cmake-build-debug/sdsl-lite/external/googletest/googletest/CMakeFiles/gtest_main.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alexa/Bureau/ConwayBromageLib/external/sdsl-lite/external/googletest/googletest/src/gtest_main.cc" "/home/alexa/Bureau/ConwayBromageLib/external/cmake-build-debug/sdsl-lite/external/googletest/googletest/CMakeFiles/gtest_main.dir/src/gtest_main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "MODE_TI"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../sdsl-lite/external/googletest/googletest/include"
+  "../sdsl-lite/external/googletest/googletest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/alexa/Bureau/ConwayBromageLib/external/cmake-build-debug/sdsl-lite/external/googletest/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
