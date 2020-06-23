53a034d5ea51375bd5791112cabda3dbf7c096f9
Alexandra <Alexandra>
2020-06-23 16:47:56 +0200

    canonic kmer list generator and sort

---
+++ canonic_generation/sortACTG.py
@@ -0,0 +1,56 @@
+import sys
+
+def sortForACTG(path):
+    file = open(path, "r")
+    list = []
+    size = 0
+    for line in file:
+        for word in line.split():
+            if word != "1":
+                size = len(word)
+                list.append(encode(word))
+    list.sort()
+    for i in list:
+        print("{}\t1".format(decode(i, size)))
+    file.close()
+
+def encode(word):
+    hash = 0
+    for letter in word:
+        hash <<= 2
+        charval = 0
+        c = letter
+        if c == "C":
+            charval = 1
+        elif c == "T":
+            charval = 2
+        elif c == "G":
+            charval = 3
+        hash += charval
+ 
+    return hash
+
+def decode(seq, size):
+    res = ""
+    lastIndex = size-1
+    for i in range(size):
+        if seq & 0x3 == 0:
+            res += 'A'
+        elif seq & 0x3 == 1:
+            res += 'C'
+        elif seq & 0x3 == 2:
+            res += 'T'
+        elif seq & 0x3 == 3:
+            res += 'G'
+        seq >>= 2
+    res = "".join(reversed(res))
+    return res
+
+def main():
+    sortForACTG(sys.argv[1])
+
+    
+if __name__ == "__main__":
+    main()
+
+    
