c7756e3ff1384671fab05b661c984dd3a088c39d
Alexandra <Alexandra>
2020-07-01 16:30:15 +0200

    Delete of unused prototypes

--- ConwayBromageLib.h
+++ ConwayBromageLib.h
@@ -5,37 +5,10 @@
 const int ALPHABET(4);  // Size of the alphabet : E = {A, C, G, T}
 const std::string NUCLEOTIDES [4] = {"A", "C", "G", "T"};
 
-//rank and select
-uint64_t rank1bit(uint64_t index, const sdsl::sd_vector<> &v);
-uint64_t rank0bit(uint64_t index, const sdsl::sd_vector<> &v);
-uint64_t select1bit(uint64_t index, const sdsl::sd_vector<> &v);
-uint64_t select0bit(uint64_t index, const sdsl::sd_vector<> &v);
-//Encode/Decode lexicographical version
-uint64_t encode(std::string word, uint64_t size);
-std::string decode(uint64_t seq, uint64_t size);
-//Encode/decode fastest ASCII version
-std::string decodeEcoli(uint64_t seq, uint64_t size);
-uint64_t encodeEcoli(std::string word, uint64_t size);
-//fromFileToSdVector variations
-sdsl::sd_vector<>fromFileToSdVector(std::string path); //can now choose the encoding format, canonical k-mer
-sdsl::sd_vector<>fromFileToSdVectorChooser(std::string path, std::string format);
-//isThisKMerHere
-bool isThisKMerHere(uint64_t Kmer, sdsl::sd_vector<> const& compressedSeq, bool encodingIsAGCT);
-//Successors of a canonical Kmer
-uint64_t getCanonical (uint64_t kmer, uint64_t kmerSize, bool encodingIsACGT);
-std::vector<uint64_t> successors(uint64_t Kmer, sdsl::sd_vector<> const& compressedSeq, bool encodingIsACGT);
 std::vector<uint64_t> successorTranslator(int successors, uint64_t compressedKMer, uint64_t size, bool format);
-//reverseComplement variations
-uint64_t reverseComplementLexico (const uint64_t mer, uint64_t kmerSize); //faster for lexicographical order
-u_int64_t reverseComplementGATBLibEcoli (const u_int64_t x, uint64_t sizeKmer); //the fastest : fastest ASCII version
-
 //Functions for tests only
 bool isCanonical (uint64_t kmer, uint64_t kmerSize, bool encodingIsACGT);
 std::vector<uint64_t> successorCounter(uint64_t compressedKMer, sdsl::sd_vector<>currentCompressedSeq, bool format);
-
-//For perf tests
-uint64_t encodeEcoliSwitchVers(std::string word, uint64_t size);
-std::string decodeEcoliIfVers(uint64_t seq, uint64_t size);
 
 //POO for KmerManipulator
 class KmerManipulator{  //abstract class
