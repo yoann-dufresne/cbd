32b657866b412d41df6fd6ba84e47a840dbe4b88
AkumalexTheExalted <57628600+AkumalexTheExalted@users.noreply.github.com>
2020-07-09 12:21:18 +0200

    Add of rightWord prototype

--- ConwayBromageLib.h
+++ ConwayBromageLib.h
@@ -133,5 +133,7 @@ public:
     friend sdsl::int_vector<> ratioForIsPresent(int ratioIn, int nbOfOnes, ConwayBromage cb);
     friend void metricForIsPresent();
 };
+//For ConwayBromage constructor, include it in ConwayBromage object as static function ?
+std::string rightWord(std::istream& kmer);
 
 #endif //CONWAYBROMAGELIB_CONWAYBROMAGELIB_H
