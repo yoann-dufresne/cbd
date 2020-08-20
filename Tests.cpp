8d10872c3a7dfb4e3cb50ea26ea7bef4068b2708
AkumalexTheExalted <57628600+AkumalexTheExalted@users.noreply.github.com>
2020-08-20 09:59:16 +0200

    Update comments

--- Tests.cpp
+++ Tests.cpp
@@ -10,6 +10,9 @@ using namespace std;
 using namespace sdsl;
 using namespace lest;
 
+/*Tests.cpp performs little unit tests for the library
+you don't need it to run*/
+
 //static const uint64_t  totalLen = 4611686018427387904;
 //static const sd_vector<>littleTestPrev = bit_vector{0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1};
 //static const sd_vector<> ret = fromFileToSdVectorChooser("./sorted_kmers.txt","ACGT");
