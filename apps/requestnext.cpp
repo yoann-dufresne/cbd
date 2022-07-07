ac8dc6ff3979898ee1abb675845e9b99992a69ca
Oceane GUINARD <oceane.guinard@pasteur.fr>
2022-07-07 16:11:32 +0200

    some work done on the next request to eliminate the need for recursive

--- apps/requestnext.cpp
+++ apps/requestnext.cpp
@@ -3,14 +3,30 @@
 #include <sdsl/vectors.hpp>
 #include <bitset>
 #include <queue>
+#include <stack>
 
-void requestnext(int range, ConwayBromageBM cb, uint64_t kmer){
+class kmerRange{
+    public:
+    uint64_t kmer;
+    int currentrange;
+    kmerRange(uint64_t kmer,int cr){
+        kmer=kmer;
+        currentrange=cr;
+    }
+    kmerRange(){}
+};
+
+void requestnext(int range, ConwayBromage& cb, uint64_t kmer){
+    stack<kmerRange> prev;
+    stack<kmerRange> next;
+    uint64_t mask=0;
+    //create a bit-mask to supress the first 2 bits of the kmer binary words
+    for(int i=0;i<cb.getKmerManipulator()->getSize()-1;i++){
+        mask=mask|(uint64_t)1<<i*2|(uint64_t)1<<i*2+1;
+    }
+
     if(range){
-        uint64_t mask=0;
-        //create a bit-mask to supress the first 2 bits of the kmer binary words
-        for(int i=0;i<cb.getKmerManipulator()->getSize()-1;i++){
-            mask=mask|(uint64_t)1<<i*2|(uint64_t)1<<i*2+1;
-        }
+        
         uint8_t tmp=cb.successors(kmer);
         std::cout<<(unsigned int)tmp<<std::endl;
         uint64_t kmertmp=kmer;
@@ -21,6 +37,37 @@ void requestnext(int range, ConwayBromageBM cb, uint64_t kmer){
                 requestnext(range-1,cb,kmertmp);
                 kmertmp=kmer;
             }
+        }
+    }
+    kmerRange ktmp;
+    while(!prev.empty()){
+        ktmp=prev.top();
+        prev.pop();
+        uint64_t kmer2=ktmp.kmer>>2;
+        uint8_t succes=cb.successors(kmer2);
+        for(int i=0;i<4;i++){
+            if(succes&(1<<i)){
+                if(range>ktmp.currentrange){
+                    uint64_t newkmer=kmer2+(uint64_t)i<<cb.getKmerManipulator()->getSize()*2;
+                    prev.push(kmerRange(newkmer,ktmp.currentrange+1));
+                }   
+            }
+
+        }
+    }
+    while(!next.empty()){
+        ktmp=next.top();
+        next.pop();
+        uint64_t kmer2=(ktmp.kmer)&mask;
+        uint8_t succes=cb.successors(kmer2);
+        for(int i=0;i<4;i++){
+            if(succes&(1<<7-i)){
+                if(range>ktmp.currentrange){
+                    uint64_t newkmer=(kmer2<<2)+i;
+                    prev.push(kmerRange(newkmer,ktmp.currentrange+1));
+                }   
+            }
+
         }
     }
 
