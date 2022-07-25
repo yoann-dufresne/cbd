07649f9af1fb7c5ddf7ea2b6335d4ad611ae720c
Oceane GUINARD <oceane.guinard@pasteur.fr>
2022-07-25 14:41:16 +0200

    first test(dont work at all)

---
+++ apps/test256.cpp
@@ -0,0 +1,10 @@
+#include <ConwayBromageLib.h>
+#include <sdsl/sd_vector.hpp>
+#include <sdsl/vectors.hpp>
+#include <bitset>
+#include <list>
+
+int main(int argc,char* argv[]){
+    uint256_t test=(uint256_t)1<<255;
+    std::cout<<test<<std::endl;
+}
