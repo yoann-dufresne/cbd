2fa990daa0dc54fce4cc71a5daa6cd4eb32afa03
Luc Blassel <luc.blassel@pasteur.fr>
2022-10-07 19:45:28 +0200

    Add benchmarking executables
    buildIndex creates a CBD from a file of sorted 31-mers, serializes it
    and saves it to disk.
    loadIndex deserializes a CBD from disk, in order to measure the loading
    time of the data structure and substract it from query benchmarks.

---
+++ apps/loadIndex.cpp
@@ -0,0 +1,43 @@
+
+#include <ConwayBromageDatastructure.h>
+
+void usage(std::string name) {
+  std::cerr << "Loads a serialized Conway-Bromage structure from disk, "
+               "for benchmarking purposes.\n"
+            << "Usage: " << name << " [OPTIONS] <file_in>\n"
+            << "\n"
+            << "[ARGUMENTS]\n"
+            << "    <file_in>  file containing the CBD\n"
+            << "\n"
+            << "[OPTIONS]\n"
+            << "    -h --help  Show this message" << std::endl;
+};
+
+int main(int argc, char* argv[]) {
+
+  if (argc != 2 && argc != 3) {
+    std::cerr << "Error: Invalid number of arguments. \n\n\n";
+    usage(argv[0]);
+    return 1;
+  }
+
+  std::string arg1;
+  std::string arg2;
+
+  if (argc == 2) {
+    arg1 = argv[1];
+    if (arg1 == "-h" || arg1 == "--help") {
+      usage(argv[0]);
+      return 0;
+    } else {
+      std::cerr << "Error: Invalid number of arguments. \n\n\n";
+      usage(argv[0]);
+      return 1;
+    }
+  }
+
+
+  // // Deserialize index
+  // KmerManipulatorACGT km = KmerManipulatorACGT(31);
+  // auto index = ConwayBromageSD::deserialize(arg1, &km);
+}
